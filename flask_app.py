aa81e0603f457e530ad4912ffdd7e7a9c4b6d4a7
Dasip <45264175+Dasip@users.noreply.github.com>
2019-04-28 21:17:58 +0300

    Рабочая версия 2.0

--- flask_app.py
+++ flask_app.py
@@ -9,30 +9,69 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 sessionStorage = {}
 
+# 1030494/7e258428c42e82021619 - waking
+# 1030494/d8b49de67720ead9fef6 - engine room
+# 1540737/8fcbe7892a1435df9265 - journal
+# 1030494/49cfd903a3238a5ff23d - reactor_broken
+# 1540737/b0efdd0a4e8959ab782d - reactor_improoved
+# 1030494/1d88827892e9f10d1ac5 - hangar 1
+# 1656841/a64bb2790416400791a8 - hangar 2
+# 1540737/2936cbd673eb75032788 - rescued
+
 # ===================================|| Локации ||===================================
-Start_loc = Location('Перекресток', [], '''Последнее, что вы помните - взрывы и скрежет металла. Корабль, должно быть, столкнулся с заброшенной космической станцией, на которой вспыхнула эпидемия неизвестного вируса.
-Вы, возможно, последний выживший член экипажа. Вам вдруг становится трудно дышать и вы понимаете, что на корабле остается мало воздуха. Под ногами вы обнаруживаете свой фонарик и кладете его в грудной карман формы.
-Вы решаете, что будете делать: ''')
-Crossing = Location('Перекресток', [], 'Вы пришли к перекрестку. Вы можете: ')
-Hangar = Location('Ангар', [], 'Вы вошли в ангар. В дальней части ангара стоит спасательная капсула, но вас от нее отрезает обесточенная шлюзовая дверь. Вы можете: ')
-Engine_room = Location('Реакторный отсек', [], 'Вы вошли в реакторный отсек. У дальней стены мигает терминал реакторной системы. Вы можете: ')
-Engine_terminal = Terminal('Реакторный терминал', '"Мунбридж Хэвикэриэр Мк II" - система заблокирована. Вы можете: ', [])
+Start_loc = Location('Перекресток', '''Последнее, что вы помните - взрывы и скрежет металла.
+Корабль, должно быть, столкнулся с заброшенной космической станцией, на которой вспыхнула эпидемия неизвестного вируса.
+Вы, возможно, последний выживший член экипажа. Вам вдруг становится трудно дышать и вы понимаете,
+что на корабле остается мало воздуха. Под рукой вы нащупываете ваш фонарик и кладете его в карман брюк. Вы решаете, что будете делать: ''')
+Crossing = Location('Перекресток', 'Вы пришли к перекрестку. Вы можете: ')
+Hangar = Location('Ангар', 'Вы вошли в ангар. В дальней части ангара стоит спасательная капсула, но вас от нее отрезает обесточенная шлюзовая дверь. Вы можете: ',)
+Engine_room = Location('Реакторный отсек', 'Вы вошли в реакторный отсек. У дальней стены мигает терминал реакторной системы. Вы можете: ')
+Engine_terminal = Terminal('Реакторный терминал', '"Мунбридж Хэвикэриэр Мк II" - система заблокирована. Вы можете: ')
+Capsule = Location('Капсула', 'Шлюз капсулы загородило крепящей балкой. Возможно, вы могли бы ее отодвинуть подходящим инструментом. Вы можете: ')
+
+Capsule.add_action(Moving('отойти от капсулы', Hangar))
+Capsule.add_action(Helpful('справка', '''<Справка> Вам выведен текст и возможные варианты действий.
+            Просто введите название действия, которое хотите произвести. Ваша задача - решить
+            все головоломки и выбраться с корабля живым.'''))
 
 Start_loc.add_action(Moving('войти в реакторный отсек', Engine_room))
 Start_loc.add_action(Moving('войти в ангар', Hangar))
+Start_loc.add_action(Helpful('справка', '''<Справка> Вам выведен текст и возможные варианты действий.
+            Просто введите название действия, которое хотите произвести. Ваша задача - решить
+            все головоломки и выбраться с корабля живым.'''))
 
 Crossing.add_action(Moving('войти в реакторный отсек', Engine_room))
 Crossing.add_action(Moving('войти в ангар', Hangar))
+Crossing.add_action(Helpful('справка', '''<Справка> Вам выведен текст и возможные варианты действий.
+            Просто введите название действия, которое хотите произвести. Ваша задача - решить
+            все головоломки и выбраться с корабля живым.'''))
 
 Engine_room.add_action(Moving('вернуться на перекресток', Crossing))
 Engine_room.add_action(Moving('подойти к терминалу', Engine_terminal))
-Engine_room.add_action(Adding('осмотреть отсек', 'Журнал дежурного инженера', 'Вы обнаружили журнал дежурного инженера реакторного отсека. В нем записан код от терминала реакторного терминала.',
-                              {'action': Hacking('запустить аварийные реакторы', 'Вы вводите коды из найденного журнала и успешно входите в систему, запуская аварийные реакторы. ', Hangar, Engine_terminal),
+Engine_room.add_action(Adding('осмотреть отсек', 'Журнал дежурного инженера',
+                              'Вы обнаружили журнал дежурного инженера реакторного отсека. В нем записан код от терминала реакторного терминала. ',
+                              {'action': Hacking('запустить аварийные реакторы', 'Вы вводите коды из найденного журнала и успешно входите в систему, запуская аварийные реакторы. ',
+                                                 Hangar, Engine_terminal, Capsule),
                                'object': Engine_terminal}))
+Engine_room.add_action(Helpful('справка', '''<Справка> Вам выведен текст и возможные варианты действий.
+            Просто введите название действия, которое хотите произвести. Ваша задача - решить
+            все головоломки и выбраться с корабля живым.'''))
 
 Engine_terminal.add_action(Moving('отойти от терминала', Engine_room))
+Engine_terminal.add_action(Helpful('справка', '''<Справка> Вам выведен текст и возможные варианты действий.
+            Просто введите название действия, которое хотите произвести. Ваша задача - решить
+            все головоломки и выбраться с корабля живым.'''))
 
 Hangar.add_action(Moving('вернуться на перекресток', Crossing))
+Hangar.add_action(Helpful('справка', '''<Справка> Вам выведен текст и возможные варианты действий.
+            Просто введите название действия, которое хотите произвести. Ваша задача - решить
+            все головоломки и выбраться с корабля живым.'''))
+Hangar.add_action(Adding('осмотреть отсек', 'магнитный грузоподъемный инструмент',
+                         'Вы обнаружили магнитный грузоподъемный инструмент. Его можно использовать для передвижения тяжелых металлических грузов. ',
+                        {'action': Cleaning('отодвинуть балку', 'Вы включаете магнит и отодвигаете огромную балку от капсулы. ', Capsule, Capsule,
+                                            'Перед вами стоит спасательная капсула - ваш путь на волю. Вы можете: ',
+                                            Rescuing('сесть в капсулу', 'Вы сели в капсулу и покинули корабль. Поймав сигналы ближайшего корабля, вы направились к нему. Вы спаслись.')),
+                         'object': Capsule}))
 # ===================================|| ======= ||===================================
 
 @app.route('/rpg', methods=['POST'])
@@ -52,51 +91,52 @@ def main2():
  
 def handle_dialog(res, req):
     
-    if not res['response']['end_session']:
-        user_id = req['session']['user_id']
-     
-        if req['session']['new']:
-            res['response']['text'] = '''Система управления транспортным кораблем модели "Мунбридж Хэвикэриэр Мк II",
-            назовите свое имя...'''
-            sessionStorage[user_id] = {
-                'first_name': None
-            }
-            return
+    user_id = req['session']['user_id']
+ 
+    if req['session']['new']:
+        res['response']['text'] = '''Система управления транспортным кораблем модели "Мунбридж Хэвикэриэр Мк II",
+        назовите свое имя...'''
+        sessionStorage[user_id] = {
+            'first_name': None
+        }
+        return
 
-        if sessionStorage[user_id]['first_name'] is None:
-            
-            first_name = get_first_name(req)
-            if first_name is None:
-                res['response']['text'] = \
-                    'Некорректные данные - повторите ввод данных (имя пользователя)...'
-                
-            else:
-                the_hero = Person(Start_loc)
-                sessionStorage[user_id]['first_name'] = first_name
-                sessionStorage[user_id]['hero'] = the_hero
-                sessionStorage[user_id]['actions'] = [i.text for i in the_hero.location.actions]
-                a = sessionStorage[user_id]['hero']
-                create_mess(res, {'text': 'Доступ открыт. Добро пожаловать, ' + first_name.title()\
-                              + '. ' + a.current_loc()['text'],
-                              'actions': sessionStorage[user_id]['actions']})
+    if sessionStorage[user_id]['first_name'] is None:
+        
+        first_name = get_first_name(req)
+        if first_name is None:
+            res['response']['text'] = \
+                'Некорректные данные - повторите ввод данных (имя пользователя)...'
             
         else:
-            action = req['request']['command']
-            for i in sessionStorage[user_id]['actions']:
-                if i in action:
-                    action = i
-                    break
-            
-            ans = sessionStorage[user_id]['hero'].activate(action)
-            create_mess(res, ans)
-            if sessionStorage[user_id]['hero'].rescued:
-                res['response']['end_session'] = True
-            sessionStorage[user_id]['actions'] = ans['actions']
+            the_hero = Person(Start_loc)
+            sessionStorage[user_id]['first_name'] = first_name
+            sessionStorage[user_id]['hero'] = the_hero
+            sessionStorage[user_id]['actions'] = [i.text for i in the_hero.location.actions]
+            a = sessionStorage[user_id]['hero']
+            create_mess(res, {'text': 'Доступ открыт. Добро пожаловать, ' + first_name.title()\
+                              + '. ' + a.current_loc()['text'],
+                              'actions': sessionStorage[user_id]['actions']
+                              })
+        
+    else:
+        action = req['request']['command']
+        for i in sessionStorage[user_id]['actions']:
+            if i in action:
+                action = i
+                break
+        
+        ans = sessionStorage[user_id]['hero'].activate(action)
+        create_mess(res, ans)
+        if sessionStorage[user_id]['hero'].rescued:
+            res['response']['end_session'] = True
+        sessionStorage[user_id]['actions'] = ans['actions']
             
 def create_mess(res, mess):
+    
     #res['response']['card'] = {}
     #res['response']['card']['type'] = 'BigImage'
-    #res['response']['card']['image_id'] = current_pict   
+    #res['response']['card']['image_id'] = mess['pict']   
     #res['response']['card']['title'] = mess['text']
     res['response']['text'] = mess['text']
     res['response']['buttons'] = create_buttons(mess)
--- mech.py
+++ mech.py
@@ -1,28 +1,58 @@
 class Rescuing():
     
-    def __init__(self, text):
+    def __init__(self, text, result_text):
         self.actype = 'rescuing'
         self.text = text
+        self.result_text = result_text
         
     def do(self, player):
-        return player.rescue()
+        return player.rescue(self.result_text)
+    
+    
+class Cleaning():
+    
+    def __init__(self, text, result_text, parent, system, parent_text, new_action):
+        self.actype = 'cleaning'
+        self.text = text
+        self.result_text = result_text
+        self.parent = parent
+        self.system = system
+        self.parent_text = parent_text
+        self.new_action = new_action
+        
+    def do(self, player):
+        self.system.text = self.parent_text
+        self.system.add_action(self.new_action)
+        return player.hack(self.result_text)
+    
+    
+class Helpful():
+    
+    def __init__(self, text, result_text):
+        self.actype = 'helping'
+        self.text = text
+        self.result_text = result_text
+        
+    def do(self, player):
+        return player.help(self.result_text)
         
         
 class Hacking():
     
-    def __init__(self, text, result_text, system, parent):
+    def __init__(self, text, result_text, system, parent, unlockable):
         self.actype = 'hacking'
         self.text = text
         self.system = system
         self.result = result_text
         self.parent = parent
+        self.unlockable = unlockable
         
     def unlock_hangar(self):
         self.system.text = 'Вы вошли в ангар. В дальней части ангара стоит спасательная капсула. Вы можете: '
-        self.system.add_action(Rescuing('сесть в спасательную капсулу'))    
+        self.system.add_action(Moving('подойти к спасательной капсуле', self.unlockable))    
         
     def do(self, player):
-        self.parent.text = 'Мунбридж Хэвикэриэр Мк II" - система функционирует. Вы можете: '
+        self.parent.text = '"Мунбридж Хэвикэриэр Мк II" - система функционирует. Вы можете: '
         self.unlock_hangar()
         return player.hack(self.result)
         
@@ -55,11 +85,10 @@ class Adding():
 
 class Location():
     
-    def __init__(self, name, items, text, actions=[]):
+    def __init__(self, name, text, actions=[]):
         self.text = text
         self.name = name
         self.actions = actions[:]
-        self.items = items[:]
         
     def add_action(self, action):
         self.actions.append(action)
@@ -115,17 +144,23 @@ class Person():
             'actions': [i.text for i in self.location.actions]
             }
     
-    def rescue(self):
+    def rescue(self, text):
         self.rescued = True
         return {
-            'text': 'Вы выбрались с корабля и направились к ближайшему спасательному судну. Вы спаслись.',
+            'text': text,
             'actions': []
+            }
+    
+    def help(self, text):
+        return {
+            'text': text,
+            'actions': [i.text for i in self.location.actions]
             }
     
     def activate(self, action):
         if action in list(map(lambda x: x.text, self.location.actions)):
             action = list(filter(lambda x: x.text == action, self.location.actions))[0]
-            if action.actype != 'moving':
+            if action.actype not in ['moving', 'helping']:
                 self.location.actions.remove(action)
             return action.do(self)
         else:
@@ -133,4 +168,4 @@ class Person():
                 'text': 'Вы не можете этого сделать. ' + self.location.inform() + '.',
                 'actions': [i.text for i in self.location.actions]
                 }
-        
+     
